2cfa281812c0a97acb4612bb6212eb0746f0343d
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-09-22 16:05:43 -0300

    adicionada aplicação web para exemplo

---
+++ tads-webdev-app1/src/main/java/tads/webdev/app1/util/EncodingFilter.java
@@ -0,0 +1,44 @@
+
+package tads.webdev.app1.util;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+
+/**
+ * taken from
+ * {@link https://dertompson.com/2007/01/29/encoding-filter-for-java-web-applications/}
+ */
+public class EncodingFilter implements Filter {
+
+    private String encoding = "utf-8";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException {
+
+        request.setCharacterEncoding(encoding);
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig)
+        throws ServletException {
+
+        String encodingParam = filterConfig.getInitParameter("encoding");
+
+        if (encodingParam != null) {
+
+            encoding = encodingParam;
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+        // nothing todo
+
+    }
+
+}
---
+++ tads-webdev-app1/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="José Nascimento &lt;joseaugustodearaujonascimento@gmail.com&gt;">
+</head>
+<body>
+  <h2>Hello World! ééé</h2>
+</body> 
+</html>
