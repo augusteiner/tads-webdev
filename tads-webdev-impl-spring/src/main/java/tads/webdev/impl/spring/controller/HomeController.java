b89a583fe0b9ab5a9dfc3fcc832fee480390029e
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2016-09-27 19:56:57 -0300

    adicionado exemplo com spring mvc

---
+++ tads-webdev-impl-spring/src/main/java/tads/webdev/impl/spring/controller/HomeController.java
@@ -0,0 +1,24 @@
+
+package tads.webdev.impl.spring.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView test(HttpServletResponse response) throws IOException {
+
+        ModelAndView m = new ModelAndView("home");
+
+        m.addObject("message", "Hello World!");
+
+        return m;
+    }
+}
